cc9704b8c1a84b23e9e5f1a7b7caed28b58a90c4
Anusha <anushakathula61@gmail.com>
2015-10-12 10:01:02 +0000

    fist commit{

---
+++ .bash_history
@@ -0,0 +1,72 @@
+git clone https://github.com/paramothers/languages.git
+git clone https://github.com/paramothers/c9.git
+git config help
+git help confi
+clear
+git help config
+git config user.name
+git config user.name "anushakathula"
+git config --global user.name "Anusha"
+git config --global user.email anushakathula61@gmail.com
+git git remote add origin https://github.com/anushakathula/sample.git
+git remote add origin https://github.com/anushakathula/sample.git
+ls
+cd c9
+git remote add origin https://github.com/anushakathula/sample.git
+git remote remove origin
+git remote add origin https://github.com/anushakathula/sample.git
+clear
+git add .
+git add --all
+ls
+git status
+git add --all .
+git status
+git commit -m "first commit"
+git push
+git remote -v
+clear 
+git remote -v
+git remote rename origin destination
+git status
+git push
+git remote add origin https://github.com/anushakathula/sample.git
+git push
+git pusll
+git pull
+git pull remote master
+git clone https://github.com/anushakathula/sample.git
+cd ..
+git clone https://github.com/anushakathula/sample.git
+clera
+clear
+cd sample
+git add .
+git status
+git commit -m "first code"
+git push
+git stastus
+git status
+git add -all .
+git add --all .
+git commit -m "deslted read me "
+git push
+clear
+ls
+cd extjs
+pwsd
+pwd
+ls /var/www
+ln -s /home/anushakathula/sample/extjs /var/www
+ls
+cd sample
+git add --all .
+ls /var/www
+git commit -m "deslted read me " 
+git push
+git commit -m "Extjs"
+cd sample
+ls
+git add --all .
+git commit -m "Extjs"
+git push
---
+++ test.js
@@ -0,0 +1,111 @@
+Ext.define('Finance.view.TransactionForm', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.transactionform',
+
+    height: 200,
+    width: 550,
+
+    layout: {
+        align: 'stretch',
+        type: 'vbox'
+    },
+
+    title: 'Transaction',
+
+    items: [
+        {
+            xtype: 'form',
+            bodyPadding: 5,
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            defaults: {
+                anchor: '100%',
+                xtype: 'textfield',
+                blankText: 'This is required',
+                labelWidth: 90
+            },
+            items: [
+                // TODO do i need this hidden field?
+                {
+                    xtype: 'hiddenfield',
+                    fieldLabel: 'Label',
+                    name: 'id'
+                },
+                // symbol, type, price, qty, datetime, notes
+                {
+                    fieldLabel: 'Symbol',
+                    name: 'symbol',
+                    itemId: 'symbol',
+                    allowBlank: false,
+                    maxLength: 10,
+                    maskRe: /([a-zA-Z0-9]+)$/   // only allow letters and numbers, no spaces
+                },
+                {
+                    fieldLabel: 'Type (B/S)',
+                    name: 'ttype',
+                    itemId: 'ttype',
+                    allowBlank: false,
+                    maxLength: 1       //TODO convert this field to a radio button
+                },
+                {
+                    fieldLabel: 'Price',
+                    name: 'price',
+                    itemId: 'price',
+                    allowBlank: false,  //TODO add a regex here to validate a double
+                    maxLength: 10
+                },
+                {
+                    fieldLabel: 'Qty',
+                    name: 'quantity',
+                    itemId: 'quantity',
+                    allowBlank: false,
+                    maxLength: 10,
+                    maskRe: /([0-9]+)$/   // int field
+                },
+                {
+                    // TODO is this needed?
+                    xtype: 'hiddenfield',
+                    fieldLabel: 'datetime',
+                    name: 'datetime'
+                },
+                {
+                    fieldLabel: 'Notes',
+                    xtype: 'textareafield',
+                    name: 'notes',
+                    itemId: 'notes',
+                    allowBlank: true
+                },
+            ]
+        }
+    ],
+
+    dockedItems: [
+        {
+            xtype: 'toolbar',
+            flex: 1,
+            dock: 'bottom',
+            ui: 'footer',
+            layout: {
+                pack: 'end',
+                type: 'hbox'
+            },
+            items: [
+                {
+                    xtype: 'button',
+                    text: 'Cancel',
+                    itemId: 'cancel',
+                    iconCls: 'cancel'
+                },
+                {
+                    xtype: 'button',
+                    text: 'Save',
+                    itemId: 'save',
+                    iconCls: 'save'
+                }
+            ]
+        }
+    ]
+
+});
